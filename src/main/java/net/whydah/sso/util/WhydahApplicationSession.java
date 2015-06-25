4fe6baca27c38f3e2141406e919a39631e1027a4
Thor Henning Hetland <totto@totto.org>
2015-06-25 12:50:53 +0200

    double checked that integrationMode flag is set on all tests with active integrations

--- src/main/java/net/whydah/sso/util/WhydahApplicationSession.java
+++ src/main/java/net/whydah/sso/util/WhydahApplicationSession.java
@@ -9,10 +9,8 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Created by totto on 23.06.15.
- *
+ * <p>
  * A thread which initiales and keep your application session running
- *
- *
  */
 public class WhydahApplicationSession {
 
@@ -22,17 +20,19 @@ public class WhydahApplicationSession {
 
     private String applicationTokenId;
     private String applicationTokenXML;
+    private static boolean integrationMode = false;
 
-    public WhydahApplicationSession(){
-        this("https://whydahdev.altrancloud.com/tokenservice/","15","33779936R6Jr47D4Hj5R6p9qT");
+
+    public WhydahApplicationSession() {
+        this("https://whydahdev.altrancloud.com/tokenservice/", "15", "33779936R6Jr47D4Hj5R6p9qT");
 
     }
 
 
-    public WhydahApplicationSession(String sts, String appId, String appSecret){
-        this.sts=sts;
-        this.appId=appId;
-        this.appSecret=appSecret;
+    public WhydahApplicationSession(String sts, String appId, String appSecret) {
+        this.sts = sts;
+        this.appId = appId;
+        this.appSecret = appSecret;
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         ScheduledFuture<?> sf = scheduler.scheduleAtFixedRate(
                 new Runnable() {
@@ -40,39 +40,42 @@ public class WhydahApplicationSession {
                         releaseWhydahConnection();
                     }
                 },
-                1,  60, TimeUnit.SECONDS );
+                1, 60, TimeUnit.SECONDS);
     }
 
 
-    public String getActiveApplicationTokenId(){
+    public String getActiveApplicationTokenId() {
         return applicationTokenId;
     }
 
-    public String getActiveApplicationToken(){
+    public String getActiveApplicationToken() {
         return applicationTokenXML;
     }
 
-    public String getSTS(){
+    public String getSTS() {
         return sts;
     }
 
 
-    private void releaseWhydahConnection(){
-        applicationTokenXML = WhydahUtil.logOnApplication(sts, appId, appSecret);
-        Long expires = Long.parseLong(ApplicationXpathHelper.getExpiresFromAppTokenXml(applicationTokenXML));
-        if (expiresBeforeNextSchedule(expires)){
-            applicationTokenXML = WhydahUtil.extendApplicationSession(sts, appId, appSecret);
-            applicationTokenId = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(applicationTokenXML);
+    private void releaseWhydahConnection() {
+        if (integrationMode) {
+            applicationTokenXML = WhydahUtil.logOnApplication(sts, appId, appSecret);
+            Long expires = Long.parseLong(ApplicationXpathHelper.getExpiresFromAppTokenXml(applicationTokenXML));
+            if (expiresBeforeNextSchedule(expires)) {
+                applicationTokenXML = WhydahUtil.extendApplicationSession(sts, appId, appSecret);
+                applicationTokenId = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(applicationTokenXML);
+            }
         }
+
 
     }
 
-    public  static boolean expiresBeforeNextSchedule(Long timestamp){
+    public static boolean expiresBeforeNextSchedule(Long timestamp) {
 
         long i = System.currentTimeMillis();
         long j = (timestamp);
-        long diffSeconds  = j-i;
-        if (diffSeconds<60){
+        long diffSeconds = j - i;
+        if (diffSeconds < 60) {
             return true;
         }
         return false;
