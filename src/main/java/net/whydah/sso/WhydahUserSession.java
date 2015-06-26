8f3d13bb824c2720d8b7ea1f745e596572173dea
Thor Henning Hetland <totto@totto.org>
2015-06-26 13:44:18 +0200

    Added logging of user session worker

--- src/main/java/net/whydah/sso/WhydahUserSession.java
+++ src/main/java/net/whydah/sso/WhydahUserSession.java
@@ -1,7 +1,10 @@
 package net.whydah.sso;
 
+import net.whydah.sso.application.ApplicationXpathHelper;
 import net.whydah.sso.user.UserCredential;
 import net.whydah.sso.user.UserXpathHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -16,6 +19,7 @@ public class WhydahUserSession {
 
         private WhydahApplicationSession was;
         private UserCredential userCredential;
+        private static final Logger log = LoggerFactory.getLogger(WhydahUserSession.class);
 
         private String userTokenId;
         private String userTokenXML;
@@ -28,6 +32,7 @@ public class WhydahUserSession {
         public WhydahUserSession(WhydahApplicationSession was,UserCredential userCredential){
             this.was=was;
             this.userCredential=userCredential;
+            initializeUserSession();
             ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
             ScheduledFuture<?> sf = scheduler.scheduleAtFixedRate(
                     new Runnable() {
@@ -48,16 +53,33 @@ public class WhydahUserSession {
         }
 
         private void renewWhydahUserConnection(){
+            log.info("Rener user session");
             userTokenXML = WhydahUtil.logOnUser(was,userCredential) ;
-            Long expires = UserXpathHelper.getTimestampFromUserTokenXml(userTokenXML)+UserXpathHelper.getLifespanFromUserTokenXml(userTokenXML);
-            if (expiresBeforeNextSchedule(expires)){
-                this.userTokenXML = WhydahUtil.extendUserSession(was,userCredential);
-                userTokenId = UserXpathHelper.getUserTokenId(this.userTokenXML);
+            if (userTokenXML==null || userTokenXML.length() < 4) {
+                log.error("Error, unable to initialize new user session, userTokenXML:"+userTokenXML);
+            } else {
+                log.info("Renew user session successfull.  userTokenXml:"+userTokenXML);
+                Long expires = UserXpathHelper.getTimestampFromUserTokenXml(userTokenXML) + UserXpathHelper.getLifespanFromUserTokenXml(userTokenXML);
+                if (expiresBeforeNextSchedule(expires)) {
+                    this.userTokenXML = WhydahUtil.extendUserSession(was, userCredential);
+                    userTokenId = UserXpathHelper.getUserTokenId(this.userTokenXML);
+                }
             }
 
         }
 
-        public  static boolean expiresBeforeNextSchedule(Long timestamp){
+    private void initializeUserSession() {
+        log.info("Initializing new user session");
+        userTokenXML = WhydahUtil.logOnUser(was, userCredential) ;
+        if (userTokenXML==null || userTokenXML.length() < 4) {
+            log.error("Error, unable to initialize new user session, userTokenXML:"+userTokenXML);
+        } else {
+            log.info("Initializing user session successfull.  userTokenXml:"+userTokenXML);
+            userTokenId = UserXpathHelper.getUserTokenId(this.userTokenXML);
+        }
+    }
+
+    public  static boolean expiresBeforeNextSchedule(Long timestamp){
 
             long i = System.currentTimeMillis();
             long j = timestamp;
