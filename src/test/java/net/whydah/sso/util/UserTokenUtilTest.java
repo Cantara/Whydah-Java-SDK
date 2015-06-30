88f6c7669900998d896c58d0e6beae9d3c27ef9a
Thor Henning Hetland <totto@totto.org>
2015-06-30 21:39:21 +0200

    enhanced verification of addind several roles test

--- src/test/java/net/whydah/sso/util/UserTokenUtilTest.java
+++ src/test/java/net/whydah/sso/util/UserTokenUtilTest.java
@@ -8,7 +8,6 @@ import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
 import net.whydah.sso.user.*;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 
@@ -27,8 +26,10 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class UserTokenUtilTest {
     private static final Logger log = getLogger(UserTokenUtilTest.class);
 
-    public static final String TEMPORARY_APPLICATION_ID = "201";//"11";
-    public static final String TEMPORARY_APPLICATION_SECRET = "bbbbbbbbbbbbbbbbbbbbbbbbb";
+    public static String TEMPORARY_APPLICATION_ID = "11";//"11";
+    public static String TEMPORARY_APPLICATION_SECRET = "6r46g3q986Ep6By7B9J46m96D";
+    public static String userName = "admin";
+    public static String password = "whydahadmin";
     private final String userAdminServiceUri = "http://localhost:9992/useradminservice";
     private final String userTokenServiceUri = "http://localhost:9998/tokenservice";
     private String myApplicationTokenID = null;
@@ -39,20 +40,22 @@ public class UserTokenUtilTest {
     private String adminUserId = null;
 
     private final String orgName = "testOrg";
-    private final String roleName = "testRoleName";
+    private final String roleName = "TestXXRoleName_"+UUID.randomUUID();
+    private final String roleName2 = "TestXXRoleName_"+UUID.randomUUID();
     private final String roleValue = "true";
 
     @Before
     public void setUp() throws Exception {
-        tokenServiceUri = UriBuilder.fromUri(userTokenServiceUri).build();
-        ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID,TEMPORARY_APPLICATION_SECRET);
-        myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
-        myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-        userCredential = new UserCredential("altranadmin", "altranadmin");
-        String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
-        adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
-        adminUserId = UserXpathHelper.getUserIdFromUserTokenXml(adminUserTokenXml);
-
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+            tokenServiceUri = UriBuilder.fromUri(userTokenServiceUri).build();
+            ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID, TEMPORARY_APPLICATION_SECRET);
+            myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
+            myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            userCredential = new UserCredential(userName, password);
+            String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
+            adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
+            adminUserId = UserXpathHelper.getUserIdFromUserTokenXml(adminUserTokenXml);
+        }
     }
 
     @After
@@ -60,53 +63,31 @@ public class UserTokenUtilTest {
 
     }
 
-    @Ignore
     @Test
-    public void testFindValidUserTokenByUserTokenId() throws Exception {
-        String orgName = "testOrg";
-        String roleName = "testRoleName";
-        String roleValue = "true";
-        UserRole role = new UserRole("altranadmin",TEMPORARY_APPLICATION_ID,orgName, roleName, roleValue);
-        List<UserRole> roles = new ArrayList<>();
-        roles.add(role);
-        List<UserRole> result = WhydahUtil.addRolesToUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, roles);
-        //Re-login after add roles
-        String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
-        adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
+    public void testAddUserRolesAndFindIt() throws Exception {
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+            String roleValue = "true";
+            UserRole role = new UserRole(userName, TEMPORARY_APPLICATION_ID, orgName, roleName, roleValue);
+            List<UserRole> roles = new ArrayList<>();
+            roles.add(role);
+            role = new UserRole(userName, TEMPORARY_APPLICATION_ID, orgName, roleName2, roleValue);
+            roles.add(role);
+            WhydahUtil.addRolesToUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, roles);
+            //Re-login after add roles
+            String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
+            adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
 
-        String userTokenXml = UserTokenUtil.findValidUserTokenByUserTokenId(userTokenServiceUri,myApplicationTokenID,"anythihng",adminUserTokenId);
-        assertNotNull(userTokenXml);
+            String userTokenXml = UserTokenUtil.findValidUserTokenByUserTokenId(userTokenServiceUri, myApplicationTokenID, "anythihng", adminUserTokenId);
+            assertNotNull(userTokenXml);
 
-        String expression = "/usertoken/uid";
-        String userId = UserXpathHelper.findValue(userTokenXml, expression);
-        assertEquals("altranadmin", userId);
-        UserRole[] userRoles = UserRoleXpathHelper.getUserRoleFromUserTokenXml(userTokenXml);
-        assertEquals("testRoleName", userRoles[1].getRoleName());
+            String expression = "/usertoken/uid";
+            String userId = UserXpathHelper.findValue(userTokenXml, expression);
+            assertEquals("admin", userId);
+            UserRole[] userRoles = UserRoleXpathHelper.getUserRoleFromUserTokenXml(userTokenXml);
+            assertTrue(UserXpathHelper.hasRoleFromUserToken(userTokenXml, TEMPORARY_APPLICATION_ID, roleName));
+            assertTrue(UserXpathHelper.hasRoleFromUserToken(userTokenXml, TEMPORARY_APPLICATION_ID, roleName2));
 
-    }
 
-    String addUserAndRole(){
-
-        //Use token for add user
-        String username = "_temp_username4Role_" + System.currentTimeMillis();
-        UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username,"first","last","ref",username +"@example.com","+4712345678");
-        String userTokenXml = WhydahUtil.addUser(userAdminServiceUri,myApplicationTokenID,adminUserTokenId,userIdentity);
-        assertNotNull(userTokenXml);
-        String createdUserId = UserXpathHelper.getUserIdFromUserTokenXml(userTokenXml);
-        log.debug("Created userId {}", createdUserId);
-        assertFalse(createdUserId.contains("7583278592730985723"));
-        //User is created, now add role
-
-        UserRole role = new UserRole(createdUserId,TEMPORARY_APPLICATION_ID,orgName, roleName, roleValue);
-        List<UserRole> roles = new ArrayList<>();
-        roles.add(role);
-        List<UserRole> result = WhydahUtil.addRolesToUser(userAdminServiceUri, myApplicationTokenID,adminUserTokenId, roles);
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        String roleId = result.get(0).getId();
-        assertTrue(roleId.length() > 0);
-
-        return createdUserId;
-
+        }
     }
 }
