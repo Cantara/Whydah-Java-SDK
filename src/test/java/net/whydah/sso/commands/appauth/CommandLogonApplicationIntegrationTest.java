67ee9480f4a5588f7a2b396f6dbbfb6e6d0a4540
Thor Henning Hetland <totto@totto.org>
2015-07-02 07:57:00 +0200

    Un-Ignored another systemtest

--- src/test/java/net/whydah/sso/commands/appauth/CommandLogonApplicationIntegrationTest.java
+++ src/test/java/net/whydah/sso/commands/appauth/CommandLogonApplicationIntegrationTest.java
@@ -3,8 +3,8 @@ package net.whydah.sso.commands.appauth;
 import net.whydah.sso.application.ApplicationCredential;
 import net.whydah.sso.application.ApplicationXpathHelper;
 import net.whydah.sso.user.UserCredential;
+import net.whydah.sso.util.SystemTestUtil;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 
@@ -20,8 +20,13 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class CommandLogonApplicationIntegrationTest {
     private static final Logger log = getLogger(CommandLogonApplicationIntegrationTest.class);
 
-    public static final String TEMPORARY_APPLICATION_ID = "201";//"11";
-    public static final String TEMPORARY_APPLICATION_SECRET = "33779936R6Jr47D4Hj5R6p9qT";
+    //    public static final String TEMPORARY_APPLICATION_ID = "201";//"11";
+//    public static final String TEMPORARY_APPLICATION_SECRET = "33779936R6Jr47D4Hj5R6p9qT";
+    public static String TEMPORARY_APPLICATION_ID = "11";//"11";
+    public static String TEMPORARY_APPLICATION_SECRET = "6r46g3q986Ep6By7B9J46m96D";
+    public static String userName = "admin";
+    public static String password = "whydahadmin";
+
     private final String userAdminServiceUri = "http://localhost:9992/useradminservice";
     private final String userTokenServiceUri = "http://localhost:9998/tokenservice";
     private String myApplicationTokenID = null;
@@ -39,13 +44,14 @@ public class CommandLogonApplicationIntegrationTest {
 
     }
 
-    @Ignore
     @Test
-    public void testLogonApplication() throws Exception{
-        ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID,TEMPORARY_APPLICATION_SECRET);
-        myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
-        myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-        assertTrue(myApplicationTokenID!=null && myApplicationTokenID.length()>5);
+    public void testLogonApplication() throws Exception {
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+            ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID, TEMPORARY_APPLICATION_SECRET);
+            myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
+            myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
 
+        }
     }
 }
