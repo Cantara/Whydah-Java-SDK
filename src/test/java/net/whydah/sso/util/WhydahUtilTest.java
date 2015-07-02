cb55d92a1e0051a02c5d9926fef4d17028df1264
Thor Henning Hetland <totto@totto.org>
2015-07-02 08:02:49 +0200

    Un-Ignored another systemtest - this still need some work..

--- src/test/java/net/whydah/sso/util/WhydahUtilTest.java
+++ src/test/java/net/whydah/sso/util/WhydahUtilTest.java
@@ -10,7 +10,6 @@ import net.whydah.sso.user.UserIdentityRepresentation;
 import net.whydah.sso.user.UserRole;
 import net.whydah.sso.user.UserXpathHelper;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 
@@ -26,12 +25,14 @@ import static org.slf4j.LoggerFactory.getLogger;
 /**
  * Created by baardl on 18.06.15.
  */
-@Ignore
 public class WhydahUtilTest {
     private static final Logger log = getLogger(WhydahUtilTest.class);
-
-    public static final String TEMPORARY_APPLICATION_ID = "99";//"11";
-    public static final String TEMPORARY_APPLICATION_SECRET = "33879936R6Jr47D4Hj5R6p9qT";
+    public static String TEMPORARY_APPLICATION_ID = "11";//"11";
+    public static String TEMPORARY_APPLICATION_SECRET = "6r46g3q986Ep6By7B9J46m96D";
+    public static String userName = "admin";
+    public static String password = "whydahadmin";
+    //    public static final String TEMPORARY_APPLICATION_ID = "99";//"11";
+//    public static final String TEMPORARY_APPLICATION_SECRET = "33879936R6Jr47D4Hj5R6p9qT";
     private final String userAdminServiceUri = "http://localhost:9992/useradminservice";
     private final String userTokenServiceUri = "http://localhost:9998/tokenservice";
     private String myApplicationTokenID = null;
@@ -42,15 +43,17 @@ public class WhydahUtilTest {
 
     @Before
     public void setUp() throws Exception {
-        URI tokenServiceUri = UriBuilder.fromUri(userTokenServiceUri).build();
-        ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID,TEMPORARY_APPLICATION_SECRET);
-        myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
-        myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-        userCredential = new UserCredential("", "altranadmin");
-        log.debug("Logged in service {}", myApplicationTokenID);
-        String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
-        adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
-        log.debug("Logged in admin {}", adminUserTokenId);
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+            URI tokenServiceUri = UriBuilder.fromUri(userTokenServiceUri).build();
+            ApplicationCredential appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID, TEMPORARY_APPLICATION_SECRET);
+            myAppTokenXml = new CommandLogonApplication(tokenServiceUri, appCredential).execute();
+            myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            userCredential = new UserCredential(userName, password);
+            log.debug("Logged in service {}", myApplicationTokenID);
+            String adminUserTokenXml = new CommandLogonUserByUserCredential(tokenServiceUri, myApplicationTokenID, myAppTokenXml, userCredential, UUID.randomUUID().toString()).execute();
+            adminUserTokenId = UserXpathHelper.getUserTokenId(adminUserTokenXml);
+            log.debug("Logged in admin {}", adminUserTokenId);
+        }
 
     }
 
@@ -62,46 +65,53 @@ public class WhydahUtilTest {
 
     @Test
     public void testAddUser() throws Exception {
-        //Use token for add user
-        String username = "_temp_username_" + System.currentTimeMillis();
-        UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username,"first","last","ref",username +"@example.com","+4712345678");
-        String userTokenXml = WhydahUtil.addUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, userIdentity);
-        assertNotNull(userTokenXml);
-        String createdUserName = UserXpathHelper.getUserIdFromUserTokenXml(userTokenXml);
-        assertFalse(createdUserName.contains("7583278592730985723"));
-        assertEquals(username,createdUserName);
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+            //Use token for add user
+            String username = "_temp_username_" + System.currentTimeMillis();
+            UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username, "first", "last", "ref", username + "@example.com", "+4712345678");
+            String userTokenXml = WhydahUtil.addUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, userIdentity);
+            assertNotNull(userTokenXml);
+            String createdUserName = UserXpathHelper.getUserIdFromUserTokenXml(userTokenXml);
+            assertFalse(createdUserName.contains("7583278592730985723"));
+            assertEquals(username, createdUserName);
+        }
     }
 
 
     @Test
     public void testAddRoleToUser() throws Exception {
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
 
-        //Use token for add user
-        String username = "_temp_username4Role_" + System.currentTimeMillis();
-        UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username,"first","last","ref",username +"@example.com","+4712345678");
-        String userTokenXml = WhydahUtil.addUser(userAdminServiceUri,myApplicationTokenID,adminUserTokenId,userIdentity);
-        assertNotNull(userTokenXml);
-        String createdUserId = UserXpathHelper.getUserIdFromUserTokenXml(userTokenXml);
-        assertFalse(createdUserId.contains("7583278592730985723"));
-        //User is created, now add role
-        String orgName = "testOrg";
-        String roleName = "testRoleName";
-        String roleValue = "true";
-        UserRole role = new UserRole(createdUserId,TEMPORARY_APPLICATION_ID,orgName, roleName, roleValue);
-        List<UserRole> roles = new ArrayList<>();
-        roles.add(role);
-        List<UserRole> result = WhydahUtil.addRolesToUser(userAdminServiceUri, myApplicationTokenID,adminUserTokenId, roles);
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        String roleId = result.get(0).getId();
-        assertTrue(roleId.length() > 0);
+            //Use token for add user
+            String username = "_temp_username4Role_" + System.currentTimeMillis();
+            UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username, "first", "last", "ref", username + "@example.com", "+4712345678");
+            String userTokenXml = WhydahUtil.addUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, userIdentity);
+            assertNotNull(userTokenXml);
+            String createdUserId = UserXpathHelper.getUserIdFromUserTokenXml(userTokenXml);
+            assertFalse(createdUserId.contains("7583278592730985723"));
+            //User is created, now add role
+            String orgName = "testOrg";
+            String roleName = "testRoleName";
+            String roleValue = "true";
+            UserRole role = new UserRole(createdUserId, TEMPORARY_APPLICATION_ID, orgName, roleName, roleValue);
+            List<UserRole> roles = new ArrayList<>();
+            roles.add(role);
+            List<UserRole> result = WhydahUtil.addRolesToUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, roles);
+            assertNotNull(result);
+            assertEquals(1, result.size());
+            String roleId = result.get(0).getId();
+            assertTrue(roleId.length() > 0);
+        }
 
     }
 
     @Test
     public void testListRolesForUserAndApplication() throws Exception {
-        String username = "_temp_username4Role_" + System.currentTimeMillis();
-        UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username,"first","last","ref",username +"@example.com","+4712345678");
-        String userTokenXml = WhydahUtil.addUser(userAdminServiceUri,myApplicationTokenID,adminUserTokenId,userIdentity);
+        if (!SystemTestUtil.noLocalWhydahRunning()) {
+
+            String username = "_temp_username4Role_" + System.currentTimeMillis();
+            UserIdentityRepresentation userIdentity = new UserIdentityRepresentation(username, "first", "last", "ref", username + "@example.com", "+4712345678");
+            String userTokenXml = WhydahUtil.addUser(userAdminServiceUri, myApplicationTokenID, adminUserTokenId, userIdentity);
+        }
     }
 }
