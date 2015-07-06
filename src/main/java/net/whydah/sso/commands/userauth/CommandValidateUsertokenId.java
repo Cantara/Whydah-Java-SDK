d89e7b01a58bbb376d7c351e0def0ed480b1f58e
Sherriff <erik-dev@fjas.no>
2015-07-06 13:51:22 +0200

    formatting + fix one bug in logging to make it easier to read CommandValidateUsertokenId.

--- src/main/java/net/whydah/sso/commands/userauth/CommandValidateUsertokenId.java
+++ src/main/java/net/whydah/sso/commands/userauth/CommandValidateUsertokenId.java
@@ -11,20 +11,18 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
-import static javax.ws.rs.core.Response.Status.*;
+import static javax.ws.rs.core.Response.Status.CONFLICT;
+import static javax.ws.rs.core.Response.Status.OK;
 
 /**
  * Created by totto on 12/2/14.
  */
 public class CommandValidateUsertokenId extends HystrixCommand<Boolean> {
-
     private static final Logger log = LoggerFactory.getLogger(CommandValidateUsertokenId.class);
 
-    private URI tokenServiceUri ;
-    private String myAppTokenId ;
+    private final URI tokenServiceUri ;
+    private final String myAppTokenId ;
     private String usertokenid;
-
-
 
     public CommandValidateUsertokenId(URI tokenServiceUri, String myAppTokenId, String usertokenid) {
         super(HystrixCommandGroupKey.Factory.asKey("SSOUserAuthGroup"));
@@ -38,33 +36,30 @@ public class CommandValidateUsertokenId extends HystrixCommand<Boolean> {
 
     @Override
     protected Boolean run() {
+        log.trace("{} - myAppTokenId={}, userTokenID{}", CommandValidateUsertokenId.class.getSimpleName(), myAppTokenId, usertokenid);
 
-        log.trace("CommandValidateUsertokenId - myAppTokenId={}, userTokenID{}",myAppTokenId,usertokenid);
-
-        Client tokenServiceClient = ClientBuilder.newClient();
-
-// If we get strange values...  return false
         if (usertokenid == null || usertokenid.length() < 4) {
-            log.warn("CommandValidateUsertokenId - verifyUserTokenId - Called with bogus usertokenid={}. return false", usertokenid);
+            log.warn("verifyUserTokenId - Called with bogus usertokenid={}. return false", usertokenid);
             return false;
         }
         // logonApplication();
+        Client tokenServiceClient = ClientBuilder.newClient();
         WebTarget verifyResource = tokenServiceClient.target(tokenServiceUri).path("user/" + myAppTokenId + "/validate_usertokenid/" + usertokenid);
         Response response = get(verifyResource);
         if (response.getStatus() == OK.getStatusCode()) {
-            log.info("CommandValidateUsertokenId - verifyUserTokenId - validate_usertokenid {}  result {}", "userauth/" + myAppTokenId + "/validate_usertokenid/" + usertokenid, response);
+            log.info("verifyUserTokenId - validate_usertokenid {}  result {}", verifyResource.getUri().toString(), response);
             return true;
         }
         if (response.getStatus() == CONFLICT.getStatusCode()) {
-            log.warn("CommandValidateUsertokenId - verifyUserTokenId - usertokenid not ok: {}", response);
+            log.warn("verifyUserTokenId - usertokenid not ok: {}", response);
             return false;
         }
         //retry
-        log.info("CommandValidateUsertokenId - verifyUserTokenId - retrying usertokenid ");
+        log.info("verifyUserTokenId - retrying usertokenid ");
         //logonApplication();
         response = get(verifyResource);
         boolean bolRes = response.getStatus() == OK.getStatusCode();
-        log.info("CommandValidateUsertokenId - verifyUserTokenId - validate_usertokenid {}  result {}", "user/" + myAppTokenId + "/validate_usertokenid/" + usertokenid, response);
+        log.info("verifyUserTokenId - validate_usertokenid {}  result {}", verifyResource.getUri().toString(), response);
         return bolRes;
     }
 
