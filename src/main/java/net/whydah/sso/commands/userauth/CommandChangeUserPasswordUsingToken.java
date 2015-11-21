44ab02a49235b900f824d9f1b32245e7ca0f4232
Erik Drolshammer <erik-dev@fjas.no>
2015-11-21 18:06:14 +0100

    Two new commands: CommandResetUserPassword and CommandChangeUserPasswordUsingToken

---
+++ src/main/java/net/whydah/sso/commands/userauth/CommandChangeUserPasswordUsingToken.java
@@ -0,0 +1,55 @@
+package net.whydah.sso.commands.userauth;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.slf4j.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a> 2015-11-21.
+ */
+public class CommandChangeUserPasswordUsingToken extends HystrixCommand<Response> {
+    static final String CHANGE_PASSWORD_TOKEN_KEY = "changePasswordToken";
+    //public static final String NEW_PASSWORD_KEY = "newpassword";
+    private static final Logger log = getLogger(CommandChangeUserPasswordUsingToken.class);
+    private String uibUri;
+    private String applicationtokenId;
+    private String uid;
+    private String changePasswordToken;
+    private String json;
+
+
+    public CommandChangeUserPasswordUsingToken(String uibUri, String applicationtokenId, String uid, String changePasswordToken, String json) {
+        super(HystrixCommandGroupKey.Factory.asKey("SSOAUserAuthGroup"));
+        this.uibUri = uibUri;
+        this.applicationtokenId = applicationtokenId;
+        this.uid = uid;
+        this.changePasswordToken = changePasswordToken;
+        this.json = json;
+        if (uibUri == null || applicationtokenId == null || uid == null || changePasswordToken == null || json == null) {
+            log.error("{} initialized with null-values - will fail", CommandChangeUserPasswordUsingToken.class.getSimpleName());
+        }
+    }
+
+    @Override
+    protected Response run() {
+        log.trace("{} - applicationtokenId={}, ", CommandChangeUserPasswordUsingToken.class.getSimpleName(), applicationtokenId);
+        Client client = ClientBuilder.newClient();
+        WebTarget uib = client.target(uibUri);
+        WebTarget webResource = uib.path(applicationtokenId).path("user").path(uid).path("change_password");
+        return webResource.queryParam(CHANGE_PASSWORD_TOKEN_KEY, changePasswordToken).request().post(Entity.json(json));
+    }
+
+    @Override
+    protected Response getFallback() {
+        log.warn("{} - timeout - uibUri={}", CommandChangeUserPasswordUsingToken.class.getSimpleName(), uibUri);
+        return null;
+    }
+}
---
+++ src/main/java/net/whydah/sso/commands/userauth/CommandResetUserPassword.java
@@ -0,0 +1,47 @@
+package net.whydah.sso.commands.userauth;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.slf4j.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a> 2015-11-21.
+ */
+public class CommandResetUserPassword extends HystrixCommand<Response> {
+    private static final Logger log = getLogger(CommandResetUserPassword.class);
+    private String uibUri;
+    private String applicationtokenId;
+    private String uid;
+
+    public CommandResetUserPassword(String uibUri, String applicationtokenId, String uid) {
+        super(HystrixCommandGroupKey.Factory.asKey("SSOAUserAuthGroup"));
+        this.uibUri = uibUri;
+        this.applicationtokenId = applicationtokenId;
+        this.uid = uid;
+        if (uibUri == null || applicationtokenId == null || uid == null) {
+            log.error("{} initialized with null-values - will fail", CommandResetUserPassword.class.getSimpleName());
+        }
+    }
+
+    @Override
+    protected Response run() {
+        log.trace("{} - applicationtokenId={}, ", CommandResetUserPassword.class.getSimpleName(), applicationtokenId);
+        Client client = ClientBuilder.newClient();
+        WebTarget uib = client.target(uibUri);
+        WebTarget webResource = uib.path(applicationtokenId).path("user").path(uid).path("reset_password");
+        return webResource.request().post(null);
+    }
+
+    @Override
+    protected Response getFallback() {
+        log.warn("{} - timeout - uibUri={}", CommandResetUserPassword.class.getSimpleName(), uibUri);
+        return null;
+    }
+}
