c12a5f43953041be6f36b1c43af554f3ae0e7867
Thor Henning Hetland <totto@totto.org>
2015-11-24 10:23:26 +0100

    cleanup of command, better logging

--- src/main/java/net/whydah/sso/commands/adminapi/user/CommandAddUser.java
+++ src/main/java/net/whydah/sso/commands/adminapi/user/CommandAddUser.java
@@ -12,13 +12,9 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
-import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.slf4j.LoggerFactory.getLogger;
 
-/**
- * Created by baardl on 15.06.15.
- */
 public class CommandAddUser extends HystrixCommand<String> {
     private static final Logger log = getLogger(CommandAddUser.class);
     private URI userAdminServiceUri;
@@ -27,8 +23,6 @@ public class CommandAddUser extends HystrixCommand<String> {
     private UserCredential userCredential;
     private String userIdentityJson;
 
-
-
     public CommandAddUser(URI userAdminServiceUri, String myAppTokenId, String adminUserTokenId, String userIdentityJson) {
         super(HystrixCommandGroupKey.Factory.asKey("UASUserAdminGroup"));
         this.userAdminServiceUri = userAdminServiceUri;
@@ -36,28 +30,20 @@ public class CommandAddUser extends HystrixCommand<String> {
         this.adminUserTokenId=adminUserTokenId;
         this.userIdentityJson =userIdentityJson;
         if (userAdminServiceUri == null || myAppTokenId == null || adminUserTokenId == null || userIdentityJson==null ) {
-            log.error("CommandAddUser initialized with null-values - will fail");
+            log.error("CommandAddUser initialized with null-values - will fail - userAdminServiceUri:{}, myAppTokenId:{}, adminUserTokenId:{}, userIdentityJson:{}", userAdminServiceUri, myAppTokenId, adminUserTokenId, userIdentityJson);
         }
-
     }
 
     @Override
     protected String run() {
 
         log.trace("CommandAddUser - myAppTokenId={}, adminUserTokenId={{}, userIdentityJson={}", myAppTokenId, adminUserTokenId, userIdentityJson);
-
         Client tokenServiceClient = ClientBuilder.newClient();
-
         WebTarget addUser = tokenServiceClient.target(userAdminServiceUri).path(myAppTokenId + "/" + adminUserTokenId + "/user/");
         Response response = addUser.request().post(Entity.json(userIdentityJson));
-        if (response.getStatus() == FORBIDDEN.getStatusCode()) {
-            log.warn("CommandAddUser - addUser - User authentication failed with status code " + response.getStatus());
-            return null;
-            //throw new IllegalArgumentException("Log on failed. " + ClientResponse.Status.FORBIDDEN);
-        }
         if (response.getStatus() == OK.getStatusCode()) {
             String responseXML = response.readEntity(String.class);
-            log.info("CommandAddUser - addUser - Log on OK with response {}", responseXML);
+            log.info("CommandAddUser - addUser - Create user OK with response {}", responseXML);
             return responseXML;
         }
 
