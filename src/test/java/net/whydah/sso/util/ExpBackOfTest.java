abb85bf7adec0da447424b087584f2a6c3a51394
Thor Henning Hetland <totto@totto.org>
2015-11-30 12:55:56 +0100

    added log in test

--- src/test/java/net/whydah/sso/util/ExpBackOfTest.java
+++ src/test/java/net/whydah/sso/util/ExpBackOfTest.java
@@ -7,12 +7,16 @@ import net.whydah.sso.util.backoff.BackOff;
 import net.whydah.sso.util.backoff.BackOffExecution;
 import net.whydah.sso.util.backoff.ExponentialBackOff;
 import org.junit.Test;
+import org.slf4j.Logger;
 
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class ExpBackOfTest {
 
+    private static final Logger log = getLogger(ExpBackOfTest.class);
 
     @Test
     public void testAndDocumentExpotensialBackOff() {
@@ -26,8 +30,10 @@ public class ExpBackOfTest {
             try {
                 new CommandSendThreatSignal(dummyTokerServiceURL, "myApppId", "myThreatmessage").execute();
                 i = i + 3;
+                log.debug("Execution success, i=" + 1);
             } catch (HystrixRuntimeException e) {
                 i++;
+                log.debug("Execution failed, i=" + 1);
             }
         }
     }
