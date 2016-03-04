7c6e619e97747f3769db0c1ec08f3d898769f9ee
Thor Henning Hetland <totto@totto.org>
2016-03-04 23:38:22 +0100

    import crt

---
+++ src/test/java/net/whydah/sso/util/ImportStartSSLCertificatesTest.java
@@ -0,0 +1,29 @@
+package net.whydah.sso.util;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class ImportStartSSLCertificatesTest {
+
+    @Test
+    public void testImportCACert() {
+
+        InputStream is;
+
+        try {
+            is = new FileInputStream("/src/cantara/Whydah-Java-SDK/src/resources/ca.crt");
+
+            SSLTool.ensureSslCertIsInKeystore("startssl-ca", is);
+            is.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
