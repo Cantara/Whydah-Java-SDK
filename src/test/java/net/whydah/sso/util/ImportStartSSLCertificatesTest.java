d84de3914a7791604fe2d37ccaf31871fcc339c5
Thor Henning Hetland <totto@totto.org>
2016-03-04 23:45:32 +0100

    added the rest of the startssl certificates

--- src/test/java/net/whydah/sso/util/ImportStartSSLCertificatesTest.java
+++ src/test/java/net/whydah/sso/util/ImportStartSSLCertificatesTest.java
@@ -15,9 +15,25 @@ public class ImportStartSSLCertificatesTest {
 
         try {
             is = new FileInputStream("./src/resources/ca.crt");
-
             SSLTool.ensureSslCertIsInKeystore("startssl-ca", is);
             is.close();
+
+            is = new FileInputStream("./src/resources/sub.class1.server.ca.crt");
+            SSLTool.ensureSslCertIsInKeystore("startssl-sub.class1", is);
+            is.close();
+
+            is = new FileInputStream("./src/resources/sub.class2.server.ca.crt");
+            SSLTool.ensureSslCertIsInKeystore("startssl-sub.class2", is);
+            is.close();
+
+            is = new FileInputStream("./src/resources/sub.class3.server.ca.crt");
+            SSLTool.ensureSslCertIsInKeystore("startssl-sub.class3", is);
+            is.close();
+
+            is = new FileInputStream("./src/resources/sub.class4.server.ca.crt");
+            SSLTool.ensureSslCertIsInKeystore("startssl-sub.class4", is);
+            is.close();
+
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
