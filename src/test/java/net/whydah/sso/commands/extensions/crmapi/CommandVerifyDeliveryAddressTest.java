d83514d7184d575f8d3c8892e70493d56c18d7f6
Thor Henning Hetland <totto@totto.org>
2016-04-16 19:37:01 +0200

    added signing to implementation

--- src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandVerifyDeliveryAddressTest.java
+++ src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandVerifyDeliveryAddressTest.java
@@ -10,7 +10,8 @@ public class CommandVerifyDeliveryAddressTest {
     @Ignore
     public void testCommandVerifyDeliveryAddress() throws Exception {
 
-        String testAddress = "Møllefaret 30E, 0750 Oslo, Norway";
+//        String testAddress = "Møllefaret 30E, 0750 Oslo, Norway";
+        String testAddress = "Frankfurstein+ring+105a,M%C3%BCnchen,de,80000";
         String customerJson = new CommandVerifyDeliveryAddress(testAddress).execute();
         System.out.println("Returned result: " + customerJson);
 
