9bc4b186243681388cdbf06f4370777f67702e5b
Thor Henning Hetland <totto@totto.org>
2016-04-16 19:53:24 +0200

    updated signature to command

--- src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandVerifyDeliveryAddressTest.java
+++ src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandVerifyDeliveryAddressTest.java
@@ -3,6 +3,8 @@ package net.whydah.sso.commands.extensions.crmapi;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class CommandVerifyDeliveryAddressTest {
 
 
@@ -10,10 +12,13 @@ public class CommandVerifyDeliveryAddressTest {
     @Ignore
     public void testCommandVerifyDeliveryAddress() throws Exception {
 
+        String clientid = "";
+        byte[] key = null;
 //        String testAddress = "Møllefaret 30E, 0750 Oslo, Norway";
         String testAddress = "Frankfurstein+ring+105a,M%C3%BCnchen,de,80000";
-        String customerJson = new CommandVerifyDeliveryAddress(testAddress).execute();
+        String customerJson = new CommandVerifyDeliveryAddress(testAddress, clientid, key).execute();
         System.out.println("Returned result: " + customerJson);
+        assertTrue(customerJson.length() > 100);
 
     }
 }
