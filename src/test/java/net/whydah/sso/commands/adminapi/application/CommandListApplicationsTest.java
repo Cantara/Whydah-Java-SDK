688d771ad71a68a3a6b2abc76b11c6a62819ef6e
Thor Henning Hetland <totto@totto.org>
2016-04-25 19:58:48 +0200

    more test cleanups

--- src/test/java/net/whydah/sso/commands/adminapi/application/CommandListApplicationsTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/application/CommandListApplicationsTest.java
@@ -5,9 +5,7 @@ import net.whydah.sso.application.helpers.ApplicationXpathHelper;
 import net.whydah.sso.application.mappers.ApplicationMapper;
 import net.whydah.sso.application.types.Application;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
-import net.whydah.sso.commands.appauth.CommandLogonApplicationWithStubbedFallback;
 import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
-import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredentialWithStubbedFallback;
 import net.whydah.sso.user.helpers.UserXpathHelper;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -20,55 +18,35 @@ import static org.junit.Assert.assertTrue;
 public class CommandListApplicationsTest {
 
     static SystemTestBaseConfig config;
-//    public static String userName = "admin";
-//    public static String password = "whydahadmin";
-//    private static URI tokenServiceUri;
-//    private static ApplicationCredential appCredential;
-//    private static UserCredential userCredential;
-//    private static boolean systemTest = true;
-//    private static URI userAdminServiceUri;
-//    private static String userAdminService = "http://localhost:9992/useradminservice";
-//    private static String userTokenService = "http://localhost:9998/tokenservice";
 
     @BeforeClass
     public static void setup() throws Exception {
         config = new SystemTestBaseConfig();
-//        appCredential = new ApplicationCredential("15", "MyApp", "33779936R6Jr47D4Hj5R6p9qT");
-//        tokenServiceUri = URI.create("https://no_host").build();
-//        userCredential = new UserCredential(userName, password);
-//
-//        userAdminServiceUri = URI.create("https://no_host").build();
-//
-//        if (systemTest) {
-//            tokenServiceUri = URI.create("https://whydahdev.cantara.no/tokenservice/").build();
-//            userAdminServiceUri = URI.create("https://whydahdev.cantara.no/useradminservice/").build();
-//        }
     }
 
 
     @Test
     public void testListApplicationsCommand2() throws Exception {
+        if (config.isSystemTestEnabled()) {
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+            String userticket = UUID.randomUUID().toString();
+            String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
+            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
+            assertTrue(userTokenId != null && userTokenId.length() > 5);
 
-        String myAppTokenXml = new CommandLogonApplicationWithStubbedFallback(config.tokenServiceUri, config.appCredential).execute();
-        String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-        assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
-        String userticket = UUID.randomUUID().toString();
-        String userToken = new CommandLogonUserByUserCredentialWithStubbedFallback(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
-        String userTokenId = UserXpathHelper.getUserTokenId(userToken);
-        assertTrue(userTokenId!=null && userTokenId.length()>5);
+            String applicationsJsonl = new CommandListApplications(config.userAdminServiceUri, myApplicationTokenID, userTokenId, "").execute();
+            System.out.println("applicationsJson=" + applicationsJsonl);
+            assertTrue(!applicationsJsonl.isEmpty());
 
-        String applicationsJsonl = new CommandListApplicationsWithStubbedFallback(config.userAdminServiceUri, myApplicationTokenID,userTokenId,"").execute();
-        System.out.println("applicationsJson=" + applicationsJsonl);
-        //TODO:totto Check the hard coded text ApplicationHelper.getDummyAppllicationListJson(), failed here
-        //assertTrue(applicationsJsonl.replace("\n", "").equalsIgnoreCase(ApplicationHelper.getDummyAppllicationListJson().replace("\n", "")));
-        assertTrue(!applicationsJsonl.isEmpty());
+        }
+
     }
 
     @Test
     public void testListApplicationsCommand() throws Exception {
         if (config.isSystemTestEnabled()) {
-//            tokenServiceUri = URI.create("http://localhost:9998/tokenservice/").build();
-//            userAdminServiceUri =  URI.create("http://localhost:9992/useradminservice").build();
 
             String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
             String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
