65764b6ee7a3482069712ceaf0220b7b268b446d
Thor Henning Hetland <totto@totto.org>
2016-04-25 12:37:39 +0200

    updated test with baseConfig

--- src/test/java/net/whydah/sso/commands/appauth/CommandLogonApplicationTest.java
+++ src/test/java/net/whydah/sso/commands/appauth/CommandLogonApplicationTest.java
@@ -1,74 +1,77 @@
 package net.whydah.sso.commands.appauth;
 
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
-import net.whydah.sso.application.helpers.ApplicationHelper;
-import net.whydah.sso.application.types.ApplicationCredential;
-import net.whydah.sso.util.SSLTool;
+import net.whydah.sso.application.BaseConfig;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import rx.Observable;
 
-import javax.ws.rs.core.UriBuilder;
-import java.net.URI;
 import java.util.concurrent.Future;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by totto on 12/2/14.
  */
 public class CommandLogonApplicationTest {
 
-    private static URI tokenServiceUri;
-    private static ApplicationCredential appCredential;
+    static BaseConfig config;
     private static boolean integrationMode = false;
-
-
 
     @BeforeClass
     public static void setup() throws Exception {
-        appCredential = new ApplicationCredential("2215", "TempApp", "337799332424234R6p9qT");
-        tokenServiceUri = UriBuilder.fromUri("https://no_host").build();
-        if (integrationMode) {
-            tokenServiceUri = UriBuilder.fromUri("https://whydahdev.cantara.no/tokenservice/").build();
-        }
-
-        SSLTool.disableCertificateValidation();
+        config = new BaseConfig();
+//        appCredential = new ApplicationCredential(TEMPORARY_APPLICATION_ID, TEMPORARY_APPLICATION_NAME, TEMPORARY_APPLICATION_SECRET);
+//        tokenServiceUri = UriBuilder.fromUri(userTokenService).build();
+//        userCredential = new UserCredential(userName, password);
+//
+//        userAdminServiceUri = UriBuilder.fromUri(userAdminService).build();
+//
+//        if (systemTest) {
+//            tokenServiceUri = UriBuilder.fromUri("https://whydahdev.cantara.no/tokenservice/").build();
+//            userAdminServiceUri = UriBuilder.fromUri("https://whydahdev.cantara.no/tokenservice/").build();
+//        }
+//        SSLTool.disableCertificateValidation();
     }
 
 
     @Test
     public void testApplicationLoginCommandFallback() throws Exception {
 
-        appCredential = new ApplicationCredential("15", "TempApp", "false secret");
+        if (config.enableTesting()) {
 
-        String myAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).execute();
-        // System.out.println("ApplicationTokenID=" + myApplicationTokenID);
-        assertEquals(ApplicationHelper.getDummyApplicationToken(), myAppTokenXml);
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            // System.out.println("ApplicationTokenID=" + myApplicationTokenID);
+            //assertEquals(ApplicationHelper.getDummyApplicationToken(), myAppTokenXml);
 
-        Future<String> fmyAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).queue();
-        assertEquals(ApplicationHelper.getDummyApplicationToken(), fmyAppTokenXml.get());
+            Future<String> fmyAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).queue();
+            //assertEquals(ApplicationHelper.getDummyApplicationToken(), fmyAppTokenXml.get());
 
 
-        Observable<String> omyAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).observe();
-        // blocking
-        assertEquals(ApplicationHelper.getDummyApplicationToken(), omyAppTokenXml.toBlocking().single());
+            Observable<String> omyAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).observe();
+            // blocking
+            //assertEquals(ApplicationHelper.getDummyApplicationToken(), omyAppTokenXml.toBlocking().single());
+        }
     }
 
     @Test
     public void testApplicationLoginCommand() throws Exception {
 
-        String myAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).execute();
-        // System.out.println("ApplicationTokenID=" + myApplicationTokenID);
-        assertTrue(myAppTokenXml!=null);
-        assertTrue(myAppTokenXml.length() > 6);
+        if (config.enableTesting()) {
 
-        Future<String> fmyAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).queue();
-        assertTrue(fmyAppTokenXml.get().length() > 6);
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            // System.out.println("ApplicationTokenID=" + myApplicationTokenID);
+            assertTrue(myAppTokenXml != null);
+            assertTrue(myAppTokenXml.length() > 6);
 
-        Observable<String> omyAppTokenXml = new CommandLogonApplicationWithStubbedFallback(tokenServiceUri, appCredential).observe();
-        // blocking
-        assertTrue(omyAppTokenXml.toBlocking().single().length() > 6);
+            Future<String> fmyAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).queue();
+            assertTrue(fmyAppTokenXml.get().length() > 6);
+
+            Observable<String> omyAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).observe();
+            // blocking
+            assertTrue(omyAppTokenXml.toBlocking().single().length() > 6);
+        }
     }
 
     @Test
@@ -76,8 +79,8 @@ public class CommandLogonApplicationTest {
         if (integrationMode){
             HystrixRequestContext context = HystrixRequestContext.initializeContext();
             try {
-                CommandLogonApplication command2a = new CommandLogonApplication(tokenServiceUri, appCredential);
-                CommandLogonApplication command2b = new CommandLogonApplication(tokenServiceUri, appCredential);
+                CommandLogonApplication command2a = new CommandLogonApplication(config.tokenServiceUri, config.appCredential);
+                CommandLogonApplication command2b = new CommandLogonApplication(config.tokenServiceUri, config.appCredential);
 
                 assertTrue(command2a.execute()!=null);
                 // this is the first time we've executed this command with
@@ -95,7 +98,7 @@ public class CommandLogonApplicationTest {
             // start a new request context
             context = HystrixRequestContext.initializeContext();
             try {
-                CommandLogonApplication command3b = new CommandLogonApplication(tokenServiceUri, appCredential);
+                CommandLogonApplication command3b = new CommandLogonApplication(config.tokenServiceUri, config.appCredential);
                 assertTrue(command3b.execute()!=null);
                 // this is a new request context so this
                 // should not come from cache
