f0b47e2ee23ddd2df4f08868b28e0c83bdc49bea
Thor Henning Hetland <totto@totto.org>
2016-04-26 18:35:29 +0200

    attempted to make CRMSystemtest working

--- src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandCreateCRMCustomerTest.java
+++ src/test/java/net/whydah/sso/commands/extensions/crmapi/CommandCreateCRMCustomerTest.java
@@ -1,153 +1,171 @@
 package net.whydah.sso.commands.extensions.crmapi;
 
-import static org.junit.Assert.assertTrue;
-
-import java.net.URI;
-
+import net.whydah.sso.application.SystemTestBaseConfig;
+import net.whydah.sso.application.helpers.ApplicationXpathHelper;
 import net.whydah.sso.application.types.ApplicationCredential;
-import net.whydah.sso.user.types.UserCredential;
+import net.whydah.sso.commands.appauth.CommandLogonApplication;
+import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
+import net.whydah.sso.user.helpers.UserXpathHelper;
 import net.whydah.sso.util.SSLTool;
-
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.Random;
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
 public class CommandCreateCRMCustomerTest {
-    public static String userName = "crmadmin";
-    public static String password = "my_secret_password";
-    private static URI crmServiceUri;
-    private static ApplicationCredential appCredential;
-    private static UserCredential userCredential;
-    private static boolean systemTest = true;
+    static SystemTestBaseConfig config;
 
     @BeforeClass
     public static void setup() throws Exception {
-        appCredential = new ApplicationCredential("2299", "MyApp", "33779936R6Jr47D4Hj5R6p9qT");
-        crmServiceUri = URI.create("https:/nohost/crmservice");
-        userCredential = new UserCredential(userName, password);
-
-
-        if (systemTest) {
-            crmServiceUri = URI.create("https://whydahdev.cantara.no/crmservice/");
-        }
+        config = new SystemTestBaseConfig();
     }
 
 
-    @Ignore
+    private String getUniqueuePersonRef() {
+        Random rand = new Random();
+        rand.setSeed(new java.util.Date().getTime());
+        String user = "123456" + Integer.toString(rand.nextInt(100000000));
+        return user;
+
+    }
+
     @Test
     public void testCreateCRMCustomerCommand() throws Exception {
+        if (config.isCRMCustomerExtensionSystemTestEnabled()) {
+            String myApplicationTokenID = "";
+            SSLTool.disableCertificateValidation();
+            ApplicationCredential appCredential = new ApplicationCredential(config.TEMPORARY_APPLICATION_ID, config.TEMPORARY_APPLICATION_NAME, config.TEMPORARY_APPLICATION_SECRET);
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, appCredential).execute();
+            myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            assertTrue("Unable to log on application ", myApplicationTokenID.length() > 10);
 
-        String myApplicationTokenID = "dummyAppTokenId";
-        String adminUserTokenId = "dummyAdminUserToken";
-        String personRef = "123456"; //Must be unique for test to pass
-        String personJson = "{\n" +
-                "  \"id\" : \"123456\",\n" +
-                "  \"firstname\" : \"First\",\n" +
-                "  \"lastname\" : \"Lastname\",\n" +
-                "  \"emailaddresses\" : {\n" +
-                "    \"jobb\" : {\n" +
-                "      \"emailaddress\" : \"totto@capraconsulting.no\",\n" +
-                "      \"tags\" : \"jobb, Capra\"\n" +
-                "    },\n" +
-                "    \"kobb-kunde\" : {\n" +
-                "      \"emailaddress\" : \"thor.henning.hetland@nmd.no\",\n" +
-                "      \"tags\" : \"jobb, kunde\"\n" +
-                "    },\n" +
-                "    \"community\" : {\n" +
-                "      \"emailaddress\" : \"totto@cantara.no\",\n" +
-                "      \"tags\" : \"opensource, privat, Whydah\"\n" +
-                "    },\n" +
-                "    \"hjemme\" : {\n" +
-                "      \"emailaddress\" : \"totto@tott.org\",\n" +
-                "      \"tags\" : \"hjemme, privat, OID\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"phonenumbers\" : {\n" +
-                "    \"tja\" : {\n" +
-                "      \"phonenumber\" : \"privat\",\n" +
-                "      \"tags\" : \"96909999\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"defaultAddressLabel\" : null,\n" +
-                "  \"deliveryaddresses\" : {\n" +
-                "    \"work, override\" : {\n" +
-                "      \"addressLine1\" : \"Stenersgata 2\",\n" +
-                "      \"addressLine2\" : null,\n" +
-                "      \"postalcode\" : \"0184\",\n" +
-                "      \"postalcity\" : \"Oslo\"\n" +
-                "    },\n" +
-                "    \"home\" : {\n" +
-                "      \"addressLine1\" : \"Møllefaret 30E\",\n" +
-                "      \"addressLine2\" : null,\n" +
-                "      \"postalcode\" : \"0750\",\n" +
-                "      \"postalcity\" : \"Oslo\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
-        SSLTool.disableCertificateValidation();
-        String crmCustomerId = new CommandCreateCRMCustomer(crmServiceUri, myApplicationTokenID, adminUserTokenId, personRef, personJson).execute();
-        System.out.println("Returned CRM customer id: " + crmCustomerId);
-        assertTrue(crmCustomerId != null);
-        assertTrue(crmCustomerId.equals(personRef));
+            String userticket = UUID.randomUUID().toString();
+            String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
+            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
+            assertTrue("Unable to logon user ", userTokenId.length() > 10);
+
+
+            String personRef = getUniqueuePersonRef(); //Must be unique for test to pass
+            String personJson = "{\n" +
+                    "  \"id\" : \"" + personRef + "\",\n" +
+                    "  \"firstname\" : \"First\",\n" +
+                    "  \"lastname\" : \"Lastname\",\n" +
+                    "  \"emailaddresses\" : {\n" +
+                    "    \"jobb\" : {\n" +
+                    "      \"emailaddress\" : \"totto@capraconsulting.no\",\n" +
+                    "      \"tags\" : \"jobb, Capra\"\n" +
+                    "    },\n" +
+                    "    \"kobb-kunde\" : {\n" +
+                    "      \"emailaddress\" : \"thor.henning.hetland@nmd.no\",\n" +
+                    "      \"tags\" : \"jobb, kunde\"\n" +
+                    "    },\n" +
+                    "    \"community\" : {\n" +
+                    "      \"emailaddress\" : \"totto@cantara.no\",\n" +
+                    "      \"tags\" : \"opensource, privat, Whydah\"\n" +
+                    "    },\n" +
+                    "    \"hjemme\" : {\n" +
+                    "      \"emailaddress\" : \"totto@totto.org\",\n" +
+                    "      \"tags\" : \"hjemme, privat, OID\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"phonenumbers\" : {\n" +
+                    "    \"tja\" : {\n" +
+                    "      \"phonenumber\" : \"privat\",\n" +
+                    "      \"tags\" : \"96909999\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"defaultAddressLabel\" : null,\n" +
+                    "  \"deliveryaddresses\" : {\n" +
+                    "    \"work, override\" : {\n" +
+                    "      \"addressLine1\" : \"Stenersgata 2\",\n" +
+                    "      \"addressLine2\" : null,\n" +
+                    "      \"postalcode\" : \"0184\",\n" +
+                    "      \"postalcity\" : \"Oslo\"\n" +
+                    "    },\n" +
+                    "    \"home\" : {\n" +
+                    "      \"addressLine1\" : \"Møllefaret 30E\",\n" +
+                    "      \"addressLine2\" : null,\n" +
+                    "      \"postalcode\" : \"0750\",\n" +
+                    "      \"postalcity\" : \"Oslo\"\n" +
+                    "    }\n" +
+                    "  }\n" +
+                    "}";
+            String crmCustomerId = new CommandCreateCRMCustomer(config.crmServiceUri, myApplicationTokenID, userTokenId, personRef, personJson).execute();
+            System.out.println("Returned CRM customer id: " + crmCustomerId);
+            assertTrue(crmCustomerId != null);
+            assertTrue(crmCustomerId.equals(personRef));
+        }
 
     }
 
     @Test
-    @Ignore
     public void testCreateCRMCustomerCommand_NoId() throws Exception {
 
-        String myApplicationTokenID = "dummyAppTokenId";
-        String adminUserTokenId = "dummyAdminUserToken";
-        String personRef = null;
-        String personJson = "{\n" +
-                "  \"id\" : \"123456\",\n" +
-                "  \"firstname\" : \"First\",\n" +
-                "  \"lastname\" : \"Lastname\",\n" +
-                "  \"emailaddresses\" : {\n" +
-                "    \"jobb\" : {\n" +
-                "      \"emailaddress\" : \"totto@capraconsulting.no\",\n" +
-                "      \"tags\" : \"jobb, Capra\"\n" +
-                "    },\n" +
-                "    \"kobb-kunde\" : {\n" +
-                "      \"emailaddress\" : \"thor.henning.hetland@nmd.no\",\n" +
-                "      \"tags\" : \"jobb, kunde\"\n" +
-                "    },\n" +
-                "    \"community\" : {\n" +
-                "      \"emailaddress\" : \"totto@cantara.no\",\n" +
-                "      \"tags\" : \"opensource, privat, Whydah\"\n" +
-                "    },\n" +
-                "    \"hjemme\" : {\n" +
-                "      \"emailaddress\" : \"totto@tott.org\",\n" +
-                "      \"tags\" : \"hjemme, privat, OID\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"phonenumbers\" : {\n" +
-                "    \"tja\" : {\n" +
-                "      \"phonenumber\" : \"privat\",\n" +
-                "      \"tags\" : \"96909999\"\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"defaultAddressLabel\" : null,\n" +
-                "  \"deliveryaddresses\" : {\n" +
-                "    \"work, override\" : {\n" +
-                "      \"addressLine1\" : \"Stenersgata 2\",\n" +
-                "      \"addressLine2\" : null,\n" +
-                "      \"postalcode\" : \"0184\",\n" +
-                "      \"postalcity\" : \"Oslo\"\n" +
-                "    },\n" +
-                "    \"home\" : {\n" +
-                "      \"addressLine1\" : \"Møllefaret 30E\",\n" +
-                "      \"addressLine2\" : null,\n" +
-                "      \"postalcode\" : \"0750\",\n" +
-                "      \"postalcity\" : \"Oslo\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
-        SSLTool.disableCertificateValidation();
-        String crmCustomerId = new CommandCreateCRMCustomer(crmServiceUri, myApplicationTokenID, adminUserTokenId, personRef, personJson).execute();
-        System.out.println("Returned CRM customer id: " + crmCustomerId);
-        assertTrue(crmCustomerId != null);
-        assertTrue(crmCustomerId.length() > 0 );
+        if (config.isCRMCustomerExtensionSystemTestEnabled()) {
+            String myApplicationTokenID = "";
+            SSLTool.disableCertificateValidation();
+            ApplicationCredential appCredential = new ApplicationCredential(config.TEMPORARY_APPLICATION_ID, config.TEMPORARY_APPLICATION_NAME, config.TEMPORARY_APPLICATION_SECRET);
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, appCredential).execute();
+            myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            assertTrue("Unable to log on application ", myApplicationTokenID.length() > 10);
 
+            String userticket = UUID.randomUUID().toString();
+            String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
+            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
+            assertTrue("Unable to log on user", userTokenId.length() > 10);
+            String personRef = null;
+            String personJson = "{\n" +
+                    "  \"id\" : \"123456\",\n" +
+                    "  \"firstname\" : \"First\",\n" +
+                    "  \"lastname\" : \"Lastname\",\n" +
+                    "  \"emailaddresses\" : {\n" +
+                    "    \"jobb\" : {\n" +
+                    "      \"emailaddress\" : \"totto@capraconsulting.no\",\n" +
+                    "      \"tags\" : \"jobb, Capra\"\n" +
+                    "    },\n" +
+                    "    \"kobb-kunde\" : {\n" +
+                    "      \"emailaddress\" : \"thor.henning.hetland@nmd.no\",\n" +
+                    "      \"tags\" : \"jobb, kunde\"\n" +
+                    "    },\n" +
+                    "    \"community\" : {\n" +
+                    "      \"emailaddress\" : \"totto@cantara.no\",\n" +
+                    "      \"tags\" : \"opensource, privat, Whydah\"\n" +
+                    "    },\n" +
+                    "    \"hjemme\" : {\n" +
+                    "      \"emailaddress\" : \"totto@tott.org\",\n" +
+                    "      \"tags\" : \"hjemme, privat, OID\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"phonenumbers\" : {\n" +
+                    "    \"tja\" : {\n" +
+                    "      \"phonenumber\" : \"privat\",\n" +
+                    "      \"tags\" : \"96909999\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"defaultAddressLabel\" : null,\n" +
+                    "  \"deliveryaddresses\" : {\n" +
+                    "    \"work, override\" : {\n" +
+                    "      \"addressLine1\" : \"Stenersgata 2\",\n" +
+                    "      \"addressLine2\" : null,\n" +
+                    "      \"postalcode\" : \"0184\",\n" +
+                    "      \"postalcity\" : \"Oslo\"\n" +
+                    "    },\n" +
+                    "    \"home\" : {\n" +
+                    "      \"addressLine1\" : \"Møllefaret 30E\",\n" +
+                    "      \"addressLine2\" : null,\n" +
+                    "      \"postalcode\" : \"0750\",\n" +
+                    "      \"postalcity\" : \"Oslo\"\n" +
+                    "    }\n" +
+                    "  }\n" +
+                    "}";
+            String crmCustomerId = new CommandCreateCRMCustomer(config.crmServiceUri, myApplicationTokenID, userTokenId, personRef, personJson).execute();
+            System.out.println("Returned CRM customer id: " + crmCustomerId);
+            assertTrue(crmCustomerId != null);
+            assertTrue(crmCustomerId.length() > 0);
+
+        }
     }
 }
