45a7e63294cbc51fc47b4a14b4a5e6679d55497a
Thor Henning Hetland <totto@totto.org>
2016-05-09 09:24:47 +0200

    found some strangeness in handling of applications which need to be investigated

--- src/test/java/net/whydah/sso/commands/adminapi/application/CommandListApplicationsTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/application/CommandListApplicationsTest.java
@@ -6,9 +6,12 @@ import net.whydah.sso.application.types.Application;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
 import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
 import net.whydah.sso.user.helpers.UserXpathHelper;
+import net.whydah.sso.util.ApplicationModelUtil;
 import net.whydah.sso.util.SystemTestBaseConfig;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,6 +19,8 @@ import java.util.UUID;
 import static org.junit.Assert.assertTrue;
 
 public class CommandListApplicationsTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CommandListApplicationsTest.class);
 
     static SystemTestBaseConfig config;
 
@@ -65,4 +70,30 @@ public class CommandListApplicationsTest {
         }
     }
 
+    @Test
+    public void testListApplicationsCommandAndTestMaxTomeoutparams() throws Exception {
+        if (config.isSystemTestEnabled()) {
+
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+            String userticket = UUID.randomUUID().toString();
+            String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
+            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
+            assertTrue(userTokenId != null && userTokenId.length() > 5);
+
+            String applicationsJson = new CommandListApplications(config.userAdminServiceUri, myApplicationTokenID, userTokenId, "").execute();
+            System.out.println("applicationsJson=" + applicationsJson);
+            assertTrue(applicationsJson.length() > 100);
+            List<Application> applications = ApplicationMapper.fromJsonList(applicationsJson);
+            assertTrue(applications.size() > 6);
+            for (Application application : applications) {
+                log.debug(ApplicationMapper.toPrettyJson(application));
+                log.debug("Application applicationId:{} has getSecurity:{}", application.getId(), application.getSecurity());
+                log.debug("Application applicationId:{} has maxSessionTimeoutSeconds:{}", application.getId(), application.getSecurity().getMaxSessionTimeoutSeconds());
+                log.debug("Parsed: {}", ApplicationModelUtil.getParameterForApplication(ApplicationModelUtil.maxSessionTimeoutSeconds, application.getId()));
+            }
+
+        }
     }
+}
