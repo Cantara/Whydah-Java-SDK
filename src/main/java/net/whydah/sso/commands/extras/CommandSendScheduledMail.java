a219cbdba25eb26564b69ac1841752fab004d268
Thor Henning Hetland <totto@totto.org>
2016-05-12 12:55:18 +0200

    adjusted and aligned to new uas API

--- src/main/java/net/whydah/sso/commands/extras/CommandSendScheduledMail.java
+++ src/main/java/net/whydah/sso/commands/extras/CommandSendScheduledMail.java
@@ -11,15 +11,17 @@ public class CommandSendScheduledMail extends BaseHttpPostHystrixCommand<Boolean
 
     private String timestamp;
     private String emailaddress;
+    private String subject;
     private String msg;
 
-    public CommandSendScheduledMail(URI tokenServiceUri, String appTokenId, String appTokenXml, String timestamp, String emailaddress, String msg) {
-        super(tokenServiceUri, appTokenXml, appTokenId, "SSOAUserAuthGroup");
+    public CommandSendScheduledMail(URI uasServiceUri, String appTokenId, String appTokenXml, String timestamp, String emailaddress, String subject, String msg) {
+        super(uasServiceUri, appTokenXml, appTokenId, "SSOAUserAuthGroup");
 
         this.timestamp = timestamp;
         this.emailaddress = emailaddress;
+        this.subject = subject;
         this.msg = msg;
-        if (tokenServiceUri == null || appTokenXml == null || this.emailaddress == null || this.timestamp == null || this.msg == null) {
+        if (uasServiceUri == null || appTokenXml == null || this.emailaddress == null || this.timestamp == null || this.subject == null || this.msg == null) {
             log.error("{} initialized with null-values - will fail", CommandSendScheduledMail.class.getSimpleName());
         }
     }
@@ -40,6 +42,7 @@ public class CommandSendScheduledMail extends BaseHttpPostHystrixCommand<Boolean
         Map<String, String> data = new HashMap<String, String>();
         data.put("timestamp", timestamp);
         data.put("emailaddress", emailaddress);
+        data.put("subject", msg);
         data.put("emailMessage", msg);
         return data;
     }
@@ -47,6 +50,6 @@ public class CommandSendScheduledMail extends BaseHttpPostHystrixCommand<Boolean
 
     @Override
     protected String getTargetPath() {
-        return myAppTokenId + "/send_scheduled_sms";
+        return myAppTokenId + "/send_scheduled_email";
     }
 }
