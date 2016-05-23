a8cfd89d635c9d98b1ded53a6583d18de282d9c6
totto <totto@totto.org>
2016-05-23 19:37:41 +0200

    cleanup of old systemtest

--- src/test/java/net/whydah/sso/commands/adminapi/user/CommandGetUserAggregateTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/user/CommandGetUserAggregateTest.java
@@ -3,10 +3,8 @@ package net.whydah.sso.commands.adminapi.user;
 
 import net.whydah.sso.application.helpers.ApplicationXpathHelper;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
-import net.whydah.sso.commands.appauth.CommandLogonApplicationWithStubbedFallback;
 import net.whydah.sso.commands.systemtestbase.SystemTestBaseConfig;
 import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
-import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredentialWithStubbedFallback;
 import net.whydah.sso.user.helpers.UserXpathHelper;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,7 +15,7 @@ import static org.junit.Assert.assertTrue;
 
 public class CommandGetUserAggregateTest {
 
-static SystemTestBaseConfig config;
+    static SystemTestBaseConfig config;
 
     @BeforeClass
     public static void setup() throws Exception {
@@ -28,31 +26,22 @@ static SystemTestBaseConfig config;
     @Test
     public void testGetUserAggregate() throws Exception {
 
-        boolean systemtest = config.isSystemTestEnabled();
-      String myAppTokenXml;
-      if (systemtest) {
-          myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
-      } else {
-          myAppTokenXml = new CommandLogonApplicationWithStubbedFallback(config.tokenServiceUri, config.appCredential).execute();
-      }
-    	
-      String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-	    assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
-	    String userticket = UUID.randomUUID().toString();
-        String userToken;
-        if (systemtest) {
-        	userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
-        } else {
-        	userToken = new CommandLogonUserByUserCredentialWithStubbedFallback(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
-        }
-        String userTokenId = UserXpathHelper.getUserTokenId(userToken);
-        assertTrue(userTokenId != null && userTokenId.length() > 5);
+        if (config.isSystemTestEnabled()) {
+            String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            System.out.println(myAppTokenXml);
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            System.out.println(myApplicationTokenID);
+            assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+            String userticket = UUID.randomUUID().toString();
+            String userToken;
+            userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
+            String userTokenId = UserXpathHelper.getUserTokenId(userToken);
+            assertTrue(userTokenId != null && userTokenId.length() > 5);
 
-        String usersListJson;
-        if (systemtest) {
+            String usersListJson;
             usersListJson = new CommandGetUserAggregate(config.userAdminServiceUri, myApplicationTokenID, userTokenId, "useradmin").execute();
             System.out.println("userJson=" + usersListJson);
         }
-
     }
+
 }
