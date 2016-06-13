daec4d835b657968df0d4bbb9ad64132546135a7
totto <totto@totto.org>
2016-06-13 11:20:16 +0200

    Updated dependencies to latest

--- src/test/java/net/whydah/sso/commands/adminapi/user/CommandAddUserTest.java
+++ src/test/java/net/whydah/sso/commands/adminapi/user/CommandAddUserTest.java
@@ -47,7 +47,7 @@ public class CommandAddUserTest {
     private UserIdentity getTestNewUserIdentity() {
         Random rand = new Random();
         rand.setSeed(new java.util.Date().getTime());
-        UserIdentity user = new UserIdentity("TestUser-" + UUID.randomUUID().toString().replace("-", "").replace("_", "").substring(1, 10), "Mt Test", "Testesen", "0", UUID.randomUUID().toString().replace("-", "").replace("_", "").substring(1, 10) + "@getwhydah.com", "47" + Integer.toString(rand.nextInt(100000000)));
+        UserIdentity user = new UserIdentity("Test-User-" + UUID.randomUUID().toString().replace("-", "").replace("_", "").substring(1, 10), "Mt Test", "Testesen", "0", UUID.randomUUID().toString().replace("-", "").replace("_", "").substring(1, 10) + "@getwhydah.com", "47" + Integer.toString(rand.nextInt(100000000)));
         return user;
 
     }
