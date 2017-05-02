e1f84e118a391e86f1860e794eef03908699fa39
huy <misterhuydo@gmail.com>
2017-05-02 15:45:56 +0700

    minor

--- src/main/java/net/whydah/sso/commands/userauth/CommandRefreshUserTokenByUserName.java
+++ src/main/java/net/whydah/sso/commands/userauth/CommandRefreshUserTokenByUserName.java
@@ -8,17 +8,17 @@ import java.util.Map;
 
 public class CommandRefreshUserTokenByUserName extends BaseHttpPostHystrixCommand<String>{
 
-	String userTokenId;
+	String userName;
 	public CommandRefreshUserTokenByUserName(URI serviceUri, String myAppTokenId,
-			String myAppTokenXml, String userTokenId) {
+			String myAppTokenXml, String userName) {
 		super(serviceUri, myAppTokenXml, myAppTokenId, "SSOAUserAuthGroup", 3000);
-		this.userTokenId = userTokenId; 
+		this.userName = userName; 
 	}
 
 	@Override
 	protected Map<String, String> getFormParameters() {
 		Map<String, String> data = new HashMap<String, String>();
-		data.put("username", userTokenId);
+		data.put("username", userName);
 		return data;
 	}
 
