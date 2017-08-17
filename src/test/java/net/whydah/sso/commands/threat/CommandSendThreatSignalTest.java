1e1d119b127986a6a488cbb0427704b59997eb2a
totto <totto@totto.org>
2017-08-17 17:45:06 +0200

    constructor using TheatSignal Object

--- src/test/java/net/whydah/sso/commands/threat/CommandSendThreatSignalTest.java
+++ src/test/java/net/whydah/sso/commands/threat/CommandSendThreatSignalTest.java
@@ -4,11 +4,14 @@ import net.whydah.sso.application.helpers.ApplicationXpathHelper;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
 import net.whydah.sso.util.SystemTestBaseConfig;
 import net.whydah.sso.util.WhydahUtil;
+import net.whydah.sso.whydah.ThreatSignal;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -39,4 +42,25 @@ public class CommandSendThreatSignalTest {
 
     }
 
+    @Test
+    public void testCommandSendThreatSignalTest2() throws Exception {
+
+        if (config.isSystemTestEnabled()) {
+
+            String myAppTokenXml;
+            myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            Assert.assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+
+            ThreatSignal threatSignal = new ThreatSignal("Test threatsignal");
+            threatSignal.setInstant(Instant.now().toString());
+            threatSignal.setSignalEmitter("Java-SDK-SYSTEST");
+            threatSignal.setSignalSeverity("LOW");
+            threatSignal.setSource("CommandSendThreatSignalTest.class");
+
+            String threatResult = new CommandSendThreatSignal(config.tokenServiceUri, myApplicationTokenID, threatSignal).execute();
+            assertTrue(threatResult.length() == 0);
+        }
+
+    }
 }
