d654a8ab1bc8a17b2b9a84df7f29da0c5ba12ba8
totto <totto@totto.org>
2017-08-21 18:35:12 +0200

    simple convenience functions to avoid crazy logstatements

---
+++ src/test/java/net/whydah/sso/util/LoggerUtilTest.java
@@ -0,0 +1,20 @@
+package net.whydah.sso.util;
+
+import org.junit.Test;
+
+import static net.whydah.sso.util.LoggerUtil.first50;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class LoggerUtilTest {
+    @Test
+    public void testFirst50() throws Exception {
+        String text = null;
+        assertNull(first50(null));
+        text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        assertEquals(50, first50(text).length());
+        text = "bbbb";
+        assertEquals(4, first50(text).length());
+
+    }
+}
