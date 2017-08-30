5fdbf9eedcb421e1bf5a073cba9ef32f2426ae5a
totto <totto@totto.org>
2017-08-30 12:35:07 +0200

    some more test cases of threat use

--- src/test/java/net/whydah/sso/commands/threat/CommandSendThreatSignalTest.java
+++ src/test/java/net/whydah/sso/commands/threat/CommandSendThreatSignalTest.java
@@ -2,6 +2,7 @@ package net.whydah.sso.commands.threat;
 
 import net.whydah.sso.application.helpers.ApplicationXpathHelper;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
+import net.whydah.sso.session.WhydahApplicationSession;
 import net.whydah.sso.util.SystemTestBaseConfig;
 import net.whydah.sso.util.WhydahUtil;
 import net.whydah.sso.whydah.ThreatSignal;
@@ -57,6 +58,41 @@ public class CommandSendThreatSignalTest {
             threatSignal.setSource("CommandSendThreatSignalTest.class");
 
             String threatResult = new CommandSendThreatSignal(config.tokenServiceUri, myApplicationTokenID, threatSignal).execute();
+            //assertTrue(threatResult.length() == 0);
+        }
+
+    }
+
+    @Test
+    public void testCommandSendThreatSignalTest3() throws Exception {
+
+        if (config.isSystemTestEnabled()) {
+
+            String myAppTokenXml;
+            myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            Assert.assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+
+            ThreatSignal threatSignal = WhydahApplicationSession.createThreat("Threatsignal from Systests");
+
+            String threatResult = new CommandSendThreatSignal(config.tokenServiceUri, myApplicationTokenID, threatSignal).execute();
+            //assertTrue(threatResult.length() == 0);
+        }
+
+    }
+
+    @Test
+    public void testCommandSendThreatSignalTest4() throws Exception {
+
+        if (config.isSystemTestEnabled()) {
+
+            String myAppTokenXml;
+            myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
+            String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
+            Assert.assertTrue(myApplicationTokenID != null && myApplicationTokenID.length() > 5);
+            WhydahApplicationSession applicationSession = WhydahApplicationSession.getInstance(config.tokenServiceUri.toString(), config.appCredential.getApplicationID(), config.appCredential.getApplicationName(), config.appCredential.getApplicationSecret());
+
+            applicationSession.reportThreatSignal("TestSignal from SYStest");
             //assertTrue(threatResult.length() == 0);
         }
 
