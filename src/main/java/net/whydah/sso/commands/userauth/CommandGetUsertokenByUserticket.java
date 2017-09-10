504d7c37a138a12fbf7df917daaf0c328cfc8395
totto <totto@totto.org>
2017-09-10 14:23:22 +0200

    minor cleanup

--- src/main/java/net/whydah/sso/commands/userauth/CommandGetUsertokenByUserticket.java
+++ src/main/java/net/whydah/sso/commands/userauth/CommandGetUsertokenByUserticket.java
@@ -1,14 +1,13 @@
 package net.whydah.sso.commands.userauth;
 
+import net.whydah.sso.commands.baseclasses.BaseHttpPostHystrixCommand;
+import net.whydah.sso.util.ExceptionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-
-import net.whydah.sso.commands.baseclasses.BaseHttpPostHystrixCommand;
-import net.whydah.sso.util.ExceptionUtil;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CommandGetUsertokenByUserticket extends BaseHttpPostHystrixCommand<String> {
 
@@ -16,14 +15,10 @@ public class CommandGetUsertokenByUserticket extends BaseHttpPostHystrixCommand<
 
     
     private String userticket;
-
-
-
+    int retryCnt = 0;
 
     public CommandGetUsertokenByUserticket(URI tokenServiceUri,String myAppTokenId,String myAppTokenXml,String userticket) {
-    	super(tokenServiceUri, myAppTokenXml, myAppTokenId,"SSOAUserAuthGroup");
-        
-        
+        super(tokenServiceUri, myAppTokenXml, myAppTokenId, "SSOAUserAuthGroup", 1500);
         this.userticket=userticket;
         
         if (tokenServiceUri == null || myAppTokenId == null || myAppTokenXml == null || userticket == null) {
@@ -32,56 +27,7 @@ public class CommandGetUsertokenByUserticket extends BaseHttpPostHystrixCommand<
 
     }
 
-//    @Override
-//    protected String run() {
-//
-//        log.trace("CommandGetUsertokenByUserticket - whydahServiceUri={} myAppTokenId={}", tokenServiceUri.toString(), myAppTokenId);
-//        String responseXML = null;
-//
-//        Client tokenServiceClient = ClientBuilder.newClient();
-//
-//        WebTarget userTokenResource = tokenServiceClient.target(tokenServiceUri).path("user/" + myAppTokenId + "/get_usertoken_by_userticket");
-//        log.trace("CommandGetUsertokenByUserticket - getUserTokenByUserTicket - ticket: {} apptoken: {}",userticket,myAppTokenXml);
-//        Form formData = new Form();
-//        formData.param("apptoken", myAppTokenXml);
-//        formData.param("userticket", userticket);
-//
-//        Response response = userTokenResource.request().post(Entity.entity(formData, MediaType.APPLICATION_FORM_URLENCODED_TYPE),Response.class);
-//        if (response.getStatus() == FORBIDDEN.getStatusCode()) {
-//            log.warn("CommandGetUsertokenByUserticket - getUserTokenByUserTicket failed");
-//            throw new IllegalArgumentException("CommandGetUsertokenByUserticket - getUserTokenByUserTicket failed.");
-//        }
-//        if (response.getStatus() == OK.getStatusCode()) {
-//            responseXML = response.readEntity(String.class);
-//            log.debug("CommandGetUsertokenByUserticket - Response OK with XML: {}", responseXML);
-//        } else {
-//            //retry
-//            response =userTokenResource.request().post(Entity.entity(formData, MediaType.APPLICATION_FORM_URLENCODED_TYPE),Response.class);
-//            if (response.getStatus() == OK.getStatusCode()) {
-//                responseXML = response.readEntity(String.class);
-//                log.debug("CommandGetUsertokenByUserticket - Response OK with XML: {}", responseXML);
-//            }
-//        }
-//
-//        if (responseXML == null) {
-//            String authenticationFailedMessage = ExceptionUtil.printableUrlErrorMessage("User authentication failed", userTokenResource, response);
-//            log.warn(authenticationFailedMessage);
-//            throw new RuntimeException(authenticationFailedMessage);
-//        }
-//
-//        return responseXML;
-//
-//
-//    }
-//
-//    @Override
-//    protected String getFallback() {
-//
-//        log.warn("CommandGetUsertokenByUserticket - fallback - whydahServiceUri={}", tokenServiceUri.toString());
-//        return null;
-//    }
-    
-    int retryCnt = 0;
+
     @Override
     protected String dealWithFailedResponse(String responseBody, int statusCode) {
     	if (statusCode == java.net.HttpURLConnection.HTTP_FORBIDDEN) {
@@ -108,10 +54,8 @@ public class CommandGetUsertokenByUserticket extends BaseHttpPostHystrixCommand<
     
 	@Override
 	protected String getTargetPath() {
-		// TODO Auto-generated method stub
 		return "user/" + myAppTokenId + "/get_usertoken_by_userticket";
 	}
-
 
 
 }
