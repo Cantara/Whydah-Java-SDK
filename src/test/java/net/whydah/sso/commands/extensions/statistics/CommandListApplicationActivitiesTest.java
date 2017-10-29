262a86742e34c7083f7c293702801846e22d2d93
totto <totto@totto.org>
2017-10-29 09:03:03 +0100

    cleanup test

--- src/test/java/net/whydah/sso/commands/extensions/statistics/CommandListApplicationActivitiesTest.java
+++ src/test/java/net/whydah/sso/commands/extensions/statistics/CommandListApplicationActivitiesTest.java
@@ -1,20 +1,21 @@
 package net.whydah.sso.commands.extensions.statistics;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.UUID;
-
 import net.whydah.sso.application.helpers.ApplicationXpathHelper;
+import net.whydah.sso.application.types.ApplicationTokenID;
 import net.whydah.sso.commands.appauth.CommandLogonApplication;
 import net.whydah.sso.commands.userauth.CommandLogonUserByUserCredential;
+import net.whydah.sso.ddd.WhydahIdentity;
 import net.whydah.sso.user.helpers.UserXpathHelper;
-import net.whydah.sso.util.SSLTool;
+import net.whydah.sso.user.types.UserTokenID;
 import net.whydah.sso.util.SystemTestBaseConfig;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
 
 public class CommandListApplicationActivitiesTest {
     static SystemTestBaseConfig config;
@@ -31,18 +32,18 @@ public class CommandListApplicationActivitiesTest {
 
         if (config.isStatisticsExtensionSystemtestEnabled()) {
 
-            SSLTool.disableCertificateValidation();
             String myAppTokenXml = new CommandLogonApplication(config.tokenServiceUri, config.appCredential).execute();
             String myApplicationTokenID = ApplicationXpathHelper.getAppTokenIdFromAppTokenXml(myAppTokenXml);
-            assertTrue(myApplicationTokenID.length() > 10);
+            assertTrue(new ApplicationTokenID(myApplicationTokenID).isValid());
 
             String userticket = UUID.randomUUID().toString();
             String userToken = new CommandLogonUserByUserCredential(config.tokenServiceUri, myApplicationTokenID, myAppTokenXml, config.userCredential, userticket).execute();
             String userTokenId = UserXpathHelper.getUserTokenId(userToken);
             String userId = UserXpathHelper.getUserIdFromUserTokenXml(userToken);
-            assertTrue(userTokenId.length() > 10);
+            assertTrue(new UserTokenID(userTokenId).isValid());
+            assertTrue(new WhydahIdentity(userId).isValid());
 
-            String userStats = new CommandListApplicationActivities(config.statisticsServiceUri, myApplicationTokenID, "2215", userId).execute();
+            String userStats = new CommandListApplicationActivities(config.statisticsServiceUri, myApplicationTokenID, userTokenId, userId).execute();
             log.debug("Returned list of usersessions: " + userStats);
             assertTrue(userStats != null);
             assertTrue(userStats.length() > 10);
