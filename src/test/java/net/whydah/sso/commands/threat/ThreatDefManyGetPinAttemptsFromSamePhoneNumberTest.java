6a0a71d31f228d530276cc3c2f1f43db620d6e86
huy <misterhuydo@gmail.com>
2018-11-07 19:21:37 +0700

    minor

--- src/test/java/net/whydah/sso/commands/threat/ThreatDefManyGetPinAttemptsFromSamePhoneNumberTest.java
+++ src/test/java/net/whydah/sso/commands/threat/ThreatDefManyGetPinAttemptsFromSamePhoneNumberTest.java
@@ -54,7 +54,7 @@ public class ThreatDefManyGetPinAttemptsFromSamePhoneNumberTest {
 	    	}
 
 	    	try {
-				Thread.sleep(2000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
