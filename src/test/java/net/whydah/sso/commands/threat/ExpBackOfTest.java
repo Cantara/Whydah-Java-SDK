b1e899c6900745763711e5189f82bf8b18e69551
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2022-01-27 09:36:43 +0100

    Do not use deprecated constructor which relies on WAS singleton.

--- src/test/java/net/whydah/sso/commands/threat/ExpBackOfTest.java
+++ src/test/java/net/whydah/sso/commands/threat/ExpBackOfTest.java
@@ -1,18 +1,18 @@
 package net.whydah.sso.commands.threat;
 
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-import java.net.URI;
-
+import com.netflix.hystrix.exception.HystrixRuntimeException;
 import net.whydah.sso.util.backoff.BackOff;
 import net.whydah.sso.util.backoff.BackOffExecution;
 import net.whydah.sso.util.backoff.ExponentialBackOff;
-
+import net.whydah.sso.whydah.ThreatSignal;
 import org.junit.Test;
 import org.slf4j.Logger;
 
-import com.netflix.hystrix.exception.HystrixRuntimeException;
+import java.net.URI;
+import java.time.Instant;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class ExpBackOfTest {
 
@@ -28,7 +28,13 @@ public class ExpBackOfTest {
         int i = 0;
         while (i < 5) {
             try {
-                new CommandSendThreatSignal(dummyTokerServiceURL, "myApppId", "myThreatmessage").execute();
+                ThreatSignal threatSignal = new ThreatSignal("myThreatmessage");
+                threatSignal.setInstant(Instant.now().toString());
+                threatSignal.setSignalEmitter("Java-SDK-unit-test");
+                threatSignal.setSignalSeverity("LOW");
+                threatSignal.setSource("ExpBackOfTest.class");
+
+                new CommandSendThreatSignal(dummyTokerServiceURL, "myApppId", threatSignal).execute();
                 i = i + 3;
                 log.debug("Execution success, i=" + 1);
             } catch (HystrixRuntimeException e) {
